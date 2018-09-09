e2a8b6ff19eb53bf3eeb50b3e381f404bd0fff99
Leon <leon@Leons-MacBook-Pro.local>
2018-09-09 18:49:40 -0400

    first commit

---
+++ src/main/java/rocks/zipcodewilmington/casinoapplication/CasinoApplication.java
@@ -0,0 +1,14 @@
+package rocks.zipcodewilmington.casinoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author leon on 9/9/18.
+ */
+@SpringBootApplication
+public class CasinoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CasinoApplication.class, args);
+    }
+}
